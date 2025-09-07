06fe3f1767ddcd13c2fc02fa6393bd17ac932a95
Hernan Alfaro <hernana860@gmail.com>
2025-09-06 22:46:44 -0300

    .

--- WebNode/index.js
+++ WebNode/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-app.use(express.static('dist'))
 
 app.use(express.json())
+app.use(express.static('dist'))
+
 let notes = [
   {
     id: 1,
