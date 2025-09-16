f20a7f06110b4ff2f4638305deae26bae5fd81d0
Hernan Alfaro <hernana860@gmail.com>
2025-09-16 15:33:47 -0300

    test

--- WebNode/utils/config.js
+++ WebNode/utils/config.js
@@ -1,6 +1,7 @@
 
 const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.mongodb_url
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URL : process.env.mongodb_url
 
 module.exports = {
   MONGODB_URI,
