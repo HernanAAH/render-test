6f785c380d38bd56c991d9d23f6e173430b3563c
Hernan Alfaro <hernana860@gmail.com>
2025-09-19 19:56:04 -0300

    users

--- WebNode/controllers/notes.js
+++ WebNode/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async(request, response) => {
   const notes = await Note.find({})
@@ -22,12 +23,17 @@ notesRouter.get('/:id', async(request, response, next) => {
 notesRouter.post('/', async(request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined? false : body.important,
+    user:user.id
   })
   try{
     const saveNote = await note.save()
+    user.notes = user.notes.concat(saveNote._id)
+    await user.save()
     response.status(201).json(saveNote)
   }catch(exception){
     next(exception)
